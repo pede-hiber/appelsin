fd344e4b57ea143ac0ac1927505803419e4ed718
Per Edelberg <per.edelberg@hiberworld.com>
2022-02-02 11:07:54 +0100

    simple action

---
+++ .github/workflows/actions/ahello.js
@@ -0,0 +1,2 @@
+console.log('a hello!');
+console.log('The result is displayed in the Command Line Interface');
---
+++ test_scripts/hello.js
@@ -0,0 +1,6 @@
+try {
+    console.log('Success');
+}  catch (error) {
+    console.error('Error');
+  // core.setFailed(error.message);
+}
